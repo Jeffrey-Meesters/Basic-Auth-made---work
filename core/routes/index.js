9a751acbb0546330239c8b0b5d37a4f7a045e8a0
Jeffrey Meesters <jmeesters@harlemnext.com>
2019-02-19 14:36:20 +0100

    send correct status codes and better error handling

--- core/routes/index.js
+++ core/routes/index.js
@@ -9,20 +9,41 @@ const Sequelize = require('sequelize');
 const authy = require('../modules/auth').authy;
 
 router.get('/log-in', (req, res, next) => {
-    res.json({message: 'Welcome!'})
+    const user = req.user;
+    if (user) {
+        res.location('/api/users').sendStatus(200);
+    } else {
+        res.sendStatus(200);
+    }
 })
 
 router.post('/log-in', authy, (req, res, next) => {
-    res.json({message: 'Welcome! you have logged in. You can now go to /users'})
+    const user = req.currentUser;
+
+    if (user) {
+        res.location('/api/users').sendStatus(200);
+    } else {
+        // If user is not defined then something went wrong while setting the data
+        // Because when the code reaches this the user was authenticated, but not set.
+        const error = new Error("Something unexpected happened");
+        error.status = 500;
+        next(error);
+    }
 })
 
 router.post('/sign-up', (req, res, next) => {
     // Sequelize create user with the data in req.body
     // but first hash password
     const data = req.body;
-    bcrypt.hash(data.password, saltRounds, function(error, hash) {
+    bcrypt.hash(data.password, saltRounds, function(err, hash) {
 
-        if (!error) {
+        if (!err) {
+            if (data.password.length < 4) {
+                const error = new Error("Your password must be at least 4 characters long");
+                error.status = 422;
+                return next(error);
+            }
+
             data.password = hash;
 
             User.create(data).then( (user)=> {
@@ -35,23 +56,42 @@ router.post('/sign-up', (req, res, next) => {
                 // respond with validation errors
                 return res.status(422).send(err.errors);
             })
-            .catch((error) => {
+            .catch((err) => {
                 // Else something else went wrong so throw error here
-                console.log('error when creating a new user')
-                next(500)
+                const error = new Error('unexppected error happened when creating a new user:', err);
+                error.status = 500;
+                next(error)
             })
         } else {
-            res.status(500)
-            res.json(error);
+            const error = new Error('unexppected error happened while hashing:', err);
+            error.status = 500;
+            next(error)
         }
     })
 })
 
-router.get('/users', authy, (req, res, next) => {
-    User.findAll().then((users) => {
-        res.status(200);
-        res.json({data: users});
-    })
+router.get('/user', authy, (req, res, next) => {
+    const user = req.currentUser;
+
+    if (user) {
+        User.findOne({
+            where: {email: user.email}
+        }).done(function(user, error) {
+            if(error) {
+                const error = new Error("Could not find user info")
+                error.status = 404;
+                return next(404);
+            }
+
+            res.status(200).json({'data': user});
+        });
+    } else {
+        // If user is not defined then something went wrong while setting the data
+        // Because when the code reaches this the user was authenticated, but not set.
+        const error = new Error("Something unexpected happened");
+        error.status = 500;
+        next(error);
+    }
 })
 
 module.exports = router;
--- database/models/users.js
+++ database/models/users.js
@@ -14,17 +14,21 @@ module.exports = (sequelize, DataTypes) => {
                         where: {name: value},
                         attributes: ['id']
                     })
-                        .done(function(user, error) {
+                        .done(function(user, err) {
 
-                            if (error) {
+                            if (err) {
                                 // Some unexpected error occured with the find method.
+                                const error = new Error('Error when checking for uniqueness', err)
+                                error.status = 404;
                                 return next(error);
                             }
 
                             if (user) {
                                 // We found a user with this name.
                                 // Pass the error to the next method.
-                                return next('User already exists');
+                                const error = new Error("Use another username");
+                                error.status = 409;
+                                return next(error);
                             }
                             // If we got this far, the name hasn't been used yet.
                             // Call next with no arguments when validation is successful.
@@ -41,16 +45,56 @@ module.exports = (sequelize, DataTypes) => {
                 notEmpty: {
                     msg: "Email_is_required"
                 },
+                isUnique: function(value, next) {
+
+                    User.findOne({
+                        where: {email: value},
+                        attributes: ['id']
+                    })
+                        .done(function(user, err) {
+
+                            if (err) {
+                                // Some unexpected error occured with the find method.
+                                const error = new Error('Error when checking for uniqueness', err)
+                                error.status = 404;
+                                return next(error);
+                            }
+
+                            if (user) {
+                                // We found a user with this name.
+                                // Pass the error to the next method.
+                                const error = new Error("Use another email address");
+                                error.status = 409;
+                                return next(error);
+                            }
+                            // If we got this far, the name hasn't been used yet.
+                            // Call next with no arguments when validation is successful.
+                            next();
+
+                        });
+
+                },
                 isEmail: true
             },
         },
         password: {
             type: DataTypes.STRING,
+            allowNull: false,
             validate: {
                 notEmpty: {
                     msg: "Password_is_required"
-                }
+                },
             },
+        }
+    }, {
+        validate: {
+            passLenght() {
+                if (this.password.length !== 60) {
+                    // the password is hashed to a 60 digit random string
+                    // if not something has changed or went wrong
+                    throw new Error("Something unexpected went wrong creating your password, please contact website owner. Do <strong>NOT</strong> provide your password!")
+                }
+            }
         }
     })
 
